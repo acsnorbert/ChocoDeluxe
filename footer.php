37e539896a8ae15fd5bd2ea56e56b4ec18bc017e
acsnorbika <acsnorbert@turr.hu>
2025-06-03 11:49:03 +0200

    főoldal kész

--- footer.php
+++ footer.php
@@ -1,7 +1,12 @@
 </main>
         <footer>
-            <div class="container">
-                <p>"Where life meets luxury, there is chocolate."</p>
+            <div class="footer">
+                <p class="footerp"><img src="<?php echo get_template_directory_uri(); ?>/Képek/Névtelen_terv-removebg-preview.png" alt="Fehér csokoládé"></p>
+                <div class="logok">
+                <img src="<?php echo get_template_directory_uri(); ?>/Képek/2-removebg-preview.png" alt="Fehér csokoládé">
+                <img src="<?php echo get_template_directory_uri(); ?>/Képek/3-removebg-preview.png" alt="Fehér csokoládé">
+                <img src="<?php echo get_template_directory_uri(); ?>/Képek/4-removebg-preview.png" alt="Fehér csokoládé">
+                </div>
             </div>
         </footer>
         <?php wp_footer(); ?>
